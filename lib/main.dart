20a4672bca12139cd4f8c2c25896f1efba9c7ad0
cayaa <162942854+Cahyanandita@users.noreply.github.com>
2025-01-12 21:10:32 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ void main() async {
   // Supabase setup
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://jpmddqjjetfozsiqfdwg.supabase.co',
+    url: '#',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwbWRkcWpqZXRmb3pzaXFmZHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5NTczOTAsImV4cCI6MjA0ODUzMzM5MH0.-56bO25uLShUo3mRTB1M69ZVuwxFfGvoLyLw_4Z9_mQ',
+        '#',
   );
   runApp(const MainApp());
 }
